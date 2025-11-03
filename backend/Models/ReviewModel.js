7d27f9cb8a924cd361e9bf8a50714e73535d53e5
Satvika1825 <patnamsatvika@gmail.com>
2025-11-03 14:20:50 +0530

    reveiws issues

--- backend/Models/ReviewModel.js
+++ backend/Models/ReviewModel.js
@@ -4,7 +4,7 @@ const reviewSchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, ref: "product", required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
   rating: { type: Number, min: 1, max: 5, required: true },
-  comment: { type: String, required: true },
+  comment: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
